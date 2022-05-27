64c8da7cb2481cae66a4b3aed9fe782fba61da4e
Polarts <davidklein.4496@gmail.com>
2022-05-27 16:16:58 +0300

    Added sass loaders

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { platform } from "node:process"
+import './style.scss';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
